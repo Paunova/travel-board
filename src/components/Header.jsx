06172c3879368121a10d6e6824b8ca695ef74610
Simona <spaunova.sourcico@gmail.com>
2017-07-08 20:04:38 +0200

    add page title import Google fonts import Font Awesome

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+
+function Header() {
+    return <h1 className="title">Wanderlust board <FontAwesome name="globe"/></h1>;
+}
+
+export default Header;
